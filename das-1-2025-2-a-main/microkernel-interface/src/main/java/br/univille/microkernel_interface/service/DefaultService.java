bbd94e2c96497c73e50fe0f80243b8294ba82c73
Maria Eduarda <127155901+mariagranza@users.noreply.github.com>
2025-11-25 19:36:20 -0300

    Delete das-1-2025-2-a-main directory

--- das-1-2025-2-a-main/microkernel-interface/src/main/java/br/univille/microkernel_interface/service/DefaultService.java
+++
@@ -1,8 +0,0 @@
-package br.univille.microkernel_interface.service;
-
-import java.util.HashMap;
-import org.springframework.http.HttpStatus;
-
-public interface DefaultService {
-    HttpStatus doWork(HashMap<String, String> params);
-}
